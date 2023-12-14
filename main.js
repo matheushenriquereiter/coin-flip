585f171f9cffa2ee56f2aff15398ce64c5f39e27
matheushenriquereiter <mathenriquereiter@gmail.com>
2023-12-14 19:11:46 -0300

    added betting system

--- main.js
+++ main.js
@@ -5,6 +5,12 @@ import gradient from "gradient-string";
 import inquirer from "inquirer";
 import chalk from "chalk";
 
+let money = 200;
+
+const isNumber = value => {
+  return value ? !Number.isNaN(Number(value)) : false;
+};
+
 const getRandomElement = array => {
   return array[Math.floor(Math.random() * array.length)];
 };
@@ -17,6 +23,16 @@ const showIntroduction = async () =>
 
     console.log(gradient.pastel.multiline(data));
   });
+
+const randomizeStringBackground = string => {
+  const { bgBlue, bgCyan, bgRed, bgMagenta, bgYellow } = chalk;
+
+  const possibleBackgrounds = [bgBlue, bgCyan, bgRed, bgMagenta, bgYellow];
+
+  const randomBackground = getRandomElement(possibleBackgrounds);
+
+  return randomBackground(string);
+};
 
 const flipCoin = () => {
   const sides = ["Heads", "Tails"];
@@ -34,13 +50,6 @@ const askPlayerChoice = async () => {
 
   return playerChoice;
 };
-const handlePlayerChoice = isPlayerChoiceCorrect => {
-  if (isPlayerChoiceCorrect) {
-    return console.log("You won 😎😎😎\n");
-  }
-
-  console.log("You lost 💀💀💀\n");
-};
 
 const askPlayerToContinue = async () => {
   const { continuePlaying } = await inquirer.prompt({
@@ -52,21 +61,39 @@ const askPlayerToContinue = async () => {
   return continuePlaying;
 };
 
-const randomizeStringBackground = string => {
-  const { bgBlue, bgCyan, bgGreen, bgRed, bgMagenta, bgYellow } = chalk;
+const askPlayerBet = async () => {
+  const { amount } = await inquirer.prompt({
+    name: "amount",
+    type: "input",
+    message: "Enter the amount you want to bet:",
+    validate(amount) {
+      if (!isNumber(amount)) {
+        return "The amount must be a number";
+      }
 
-  const possibleBackgrounds = [
-    bgBlue,
-    bgCyan,
-    bgGreen,
-    bgRed,
-    bgMagenta,
-    bgYellow,
-  ];
+      if (Number(amount) > money) {
+        return "The amount cannot be greater than the balance";
+      }
 
-  const randomBackground = getRandomElement(possibleBackgrounds);
+      if (Number(amount) < 0) {
+        return "The amount cannot be a negative number";
+      }
 
-  return randomBackground(string);
+      return true;
+    },
+  });
+
+  return Number(amount);
+};
+
+const handlePlayerChoice = (isPlayerChoiceCorrect, playerBet) => {
+  if (isPlayerChoiceCorrect) {
+    money += playerBet;
+    return console.log(`You won ${playerBet} dollars 😎😎😎\n`);
+  }
+
+  money -= playerBet;
+  console.log(`You lost ${playerBet} dollars 💀💀💀\n`);
 };
 
 const main = async () => {
@@ -74,8 +101,15 @@ const main = async () => {
   const playerChoice = await askPlayerChoice();
   const isPlayerChoiceCorrect = randomChoice === playerChoice;
 
+  console.log(`Your current balance is ${chalk.black.bgGreen(money)} dollars`);
+  const playerBet = await askPlayerBet();
+
   console.log(`The coin landed on: ${randomizeStringBackground(randomChoice)}`);
-  handlePlayerChoice(isPlayerChoiceCorrect);
+  handlePlayerChoice(isPlayerChoiceCorrect, playerBet);
+
+  if (money <= 0) {
+    return console.log("You are broke");
+  }
 
   const continuePlaying = await askPlayerToContinue();
 
